3a90c22429b676f293a53c67bbb5de7d1cda7a99
Maisha <maisha.mir2014@gmail.com>
2024-06-19 13:55:29 -0700

    added functionality to dropdown

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,27 @@ import { useState } from 'react'
 import Nav from './components/Nav/Nav';
 import Hero from './components/Hero/Hero';
 import Select from './components/Select/Select';
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, useNavigate, Navigate} from "react-router-dom"
 import RestaurantDetails from './pages/RestaurantDetails/RestaurantDetails';
 import Restaurants from './pages/Restaurants/Restaurants';
 import NotFound from './pages/NotFound/NotFound';
 
 function App() {
 
+  const [selectedCuisine, setSelectedCuisine] = useState('')
+
+  const handleCuisineChange = (cuisine) => {
+    setSelectedCuisine(cuisine);
+    <Navigate to={`/cuisine/${cuisine}`}/>
+  }
   return (
     <BrowserRouter>
       <Nav />
       <Hero />
-      <Select/>
+      <Select onCuisineChange={handleCuisineChange} />
       <Routes>
-        <Route path="/" element={<RestaurantDetails/>}/>
-        <Route path="/cuisine/:cuisineId" element={<Restaurants />} />
+        <Route path="/" element={<Restaurants />}/>
+        <Route path="/cuisine/:slug" element={<Restaurants cuisineType={selectedCuisine} />} />
         <Route path="/restaurant/:restId" element={<RestaurantDetails />}/>
         <Route path="*" element={<NotFound />}/>
       </Routes>
--- client/src/pages/Restaurants/Restaurants.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Restaurants() {
-  return (
-    <div>ALL RESTAURANTS</div>
-  )
-}
-
-export default Restaurants
