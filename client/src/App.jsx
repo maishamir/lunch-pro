4c7e30307760234537e4f28720fdd849ec8677e3
Maisha <maisha.mir2014@gmail.com>
2024-06-19 12:12:07 -0700

    added a NotFound page for unknown urls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Select from './components/Select/Select';
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import RestaurantDetails from './pages/RestaurantDetails/RestaurantDetails';
 import Restaurants from './pages/Restaurants/Restaurants';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
 
@@ -14,8 +15,10 @@ function App() {
       <Hero />
       <Select/>
       <Routes>
+        <Route path="/" element={<RestaurantDetails/>}/>
         <Route path="/cuisine/:cuisineId" element={<Restaurants />} />
         <Route path="/restaurant/:restId" element={<RestaurantDetails />}/>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
       
 
